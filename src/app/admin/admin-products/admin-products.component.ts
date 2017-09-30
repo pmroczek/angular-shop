3af63de184ee05de5bcd61d585b240307ead3cb9
Przemysław Mroczek <przemyslaw.mroczek@solwit.com>
2017-09-30 21:41:58 +0200

    Implement product simple filtering on client side.

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,18 +1,31 @@
+import { query } from '@angular/core/src/animation/dsl';
 import { ProductService } from '../../services/product.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
-  products$;
+export class AdminProductsComponent implements OnInit, OnDestroy {
+  products: any[];
+  filteredProducts: any[];
+  subscription: Subscription;
 
   constructor(private productService: ProductService) { }
 
   ngOnInit() {
-    this.products$ = this.productService.getAll();
+    this.subscription = this.productService.getAll().subscribe(products => this.products = this.filteredProducts = products);
   }
 
+  public ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  filter(query: string) {
+    this.filteredProducts = (query) ?
+      this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) :
+      this.products;
+  }
 }
