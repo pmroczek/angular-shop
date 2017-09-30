8ad6829500e4b07406caacd7c45db0b8c1633344
Przemysław Mroczek <przemyslaw.mroczek@solwit.com>
2017-09-30 23:13:07 +0200

    Show category list. Use consistent service methods name.

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,3 +1,4 @@
+import { CategoryService } from '../services/category.service';
 import { ProductService } from '../services/product.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -8,8 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent {
   products$;
+  categories$;
 
-  constructor(private productService: ProductService) {
+  constructor(private productService: ProductService, private categoryService: CategoryService) {
     this.products$ = productService.getAll();
-   }
+    this.categories$ = categoryService.getAll();
+  }
 }
