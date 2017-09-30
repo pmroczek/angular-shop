31b6e26a5247def9c0455a3a65ec559ab85df489
Przemysław Mroczek <przemyslaw.mroczek@solwit.com>
2017-09-30 22:39:45 +0200

    Clean up admin-products.component and enable filtering on data-table

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -11,17 +11,17 @@ import { DataTableResource } from 'angular-4-data-table';
   styleUrls: ['./admin-products.component.css']
 })
 export class AdminProductsComponent implements OnInit, OnDestroy {
-  products: Product[];
-  items: Product[] = [];
-  filteredProducts: Product[];
+  allProducts: Product[];
+  currentPageProducts: Product[] = [];
   tableResourse: DataTableResource<Product>;
   subscription: Subscription;
   itemsCount: number;
+
   constructor(private productService: ProductService) { }
 
   ngOnInit() {
     this.subscription = this.productService.getAll().subscribe(products => {
-      this.products = this.filteredProducts = products;
+      this.allProducts = products;
       this.initTable(products);
     });
   }
@@ -31,23 +31,26 @@ export class AdminProductsComponent implements OnInit, OnDestroy {
   }
 
   filter(query: string) {
-    this.filteredProducts = (query) ?
-      this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) :
-      this.products;
+    let filteredProducts = (query) ?
+      this.allProducts.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) :
+      this.allProducts;
+
+    this.initTable(filteredProducts);
   }
 
   reloadItems(params) {
     if (!this.tableResourse) {
       return;
     }
+
     this.tableResourse.query(params)
-      .then(items => this.items = items);
+      .then(items => this.currentPageProducts = items);
   }
 
   private initTable(products: Product[]) {
     this.tableResourse = new DataTableResource(products);
     this.tableResourse.query({ offset: 0 })
-      .then(items => this.items = items);
+      .then(items => this.currentPageProducts = items);
     this.tableResourse.count()
       .then(count => this.itemsCount = count);
   }
