dc6ce8eb4e7dd5186f70dfa97af236de86388041
Przemysław Mroczek <przemyslaw.mroczek@solwit.com>
2017-10-02 21:52:51 +0200

    Implementation of add to cart method.

--- src/app/src/app/services/shopping-cart.service.ts
+++ src/app/src/app/services/shopping-cart.service.ts
@@ -1,14 +1,24 @@
 import { Product } from '../../../models/product';
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
+import 'rxjs/add/operator/take';
 
 @Injectable()
 export class ShoppingCartService {
 
   constructor(private db: AngularFireDatabase) { }
 
-  public addToCart(product: Product) {
-    let cart = this.getOrCreateCart();
+  public async addToCart(product: Product) {
+    let cart = await this.getOrCreateCartId();
+    let item$ = this.db.object('/shopping-carts/' + cart + '/items/' + product.$key);
+    item$
+      .take(1).subscribe(item => {
+        if (item.$exists()) {
+          item$.update({ quantity: item.quantity + 1 });
+        } else {
+          item$.set({ product: product, quantity: 1 });
+        }
+      });
   }
 
   private create() {
@@ -21,14 +31,14 @@ export class ShoppingCartService {
     return this.db.object('/shopping-cart/' + cartId);
   }
 
-  private async getOrCreateCart() {
+  private async getOrCreateCartId(): Promise<string> {
     let cartId = localStorage.getItem('cartId');
-    if (!cartId) {
-      let result = await this.create();
-      cartId = result.key;
-      localStorage.setItem('cartId', cartId);
+    if (cartId) {
+      return cartId;
     }
 
-    return this.getCart(cartId);
+    let result = await this.create();
+    localStorage.setItem('cartId', cartId);
+    return result.key;
   }
 }
